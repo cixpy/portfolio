c9d303de64c1ebdee2376e3ebcf963624c23f0c7
Cix <gabrielviniciodacosta@gmail.com>
2026-04-06 09:18:41 -0300

    feat: update image URLs in raw.json to use new hosting service

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,12 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
